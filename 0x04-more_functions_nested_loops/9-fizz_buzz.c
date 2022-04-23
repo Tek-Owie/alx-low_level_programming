f33b69e9588dc47998e166ee8a7d4023aec15807
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-04-23 03:50:45 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - prints fizz for int divisble by 3 and Buzz for 5
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 1; i < 100; i++)
+{
+if (i % 3 == 0 && i % 5 != 0)
+{
+printf("Fizz");
+}
+else if (i % 3 != 0 && i % 5 == 0)
+{
+printf("Buzz");
+}
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else
+{
+printf("%d", i);
+}
+if (i != 100)
+putchar(' ');
+else
+putchar('\n');
+}
+return (0);
+}
