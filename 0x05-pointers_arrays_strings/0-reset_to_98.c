962962f11c357851c3e81f1411d29e267f1a6ce0
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-04-25 08:50:49 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int and updates
+ * the value to 98.
+ * @*n: parameter it takes
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	int n;
+	int *n;
+
+	*n = 98;
+}
