af155675990f8b73068ab2c48410a19895fc9ff2
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-04-25 08:53:57 -0700

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -10,8 +10,5 @@
 
 void reset_to_98(int *n)
 {
-	int n;
-	int *n;
-
 	*n = 98;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -14,4 +14,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif _MAIN_H_
+#endif /*_MAIN_H_*/
