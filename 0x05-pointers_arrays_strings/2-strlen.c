41dc4301dfe4be5a3d39cdc111795ab4b780e356
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-04-26 10:49:37 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: string argument
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	_putchar(count + '0');
+}
