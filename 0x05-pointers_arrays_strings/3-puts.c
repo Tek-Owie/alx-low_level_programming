99c108bee8e731250ff6235c3f03b44616f80075
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-04-27 14:15:18 -0700

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by new line.
+ * @str: string parameter.
+ *
+ * Return: Nothing
+ */
+
+void _puts(char *str)
+{
+	_putchar("%s", str);
+	_putchar('\n');
+}
