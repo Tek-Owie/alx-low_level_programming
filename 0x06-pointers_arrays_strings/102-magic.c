7d28d24fe0d48ee7b250b7523173add5af0d5a64
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-05-15 11:48:58 -0700

    A dream doesn't become reality through magic; it takes sweat, determination and hard work

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,24 +1,7 @@
-#include <stdio.h>
-
-int main(void)
-{
-	int n;
-	int a[5];
-	int *p;
-
-	a[2] = 1024;
-
-	p = &n;
-	/*
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */
-	*(p + 5) = 98;
-	/*...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
-}
+Debugger entered--Lisp error: (invalid-read-syntax "#")
+  read(#<buffer *scratch*>)
+  elisp--eval-defun()
+  eval-defun(nil)
+  funcall-interactively(eval-defun nil)
+  call-interactively(eval-defun nil nil)
+  command-execute(eval-defun)
